dc31a52ff2e220b2889680fc7a5228bdbf574ecb
Yasmin Lisboa Provedel <158837627+yasminrlisboa@users.noreply.github.com>
2026-02-05 17:36:03 +0000

    frase certa do app

---
+++ app/layout.js
@@ -0,0 +1,6 @@
+export default function RootLayout({ children }) {
+    return (
+        <html lang="pt-BR">
+            <body>{children}</body></html>
+    );
+}
---
+++ app/page.js
@@ -0,0 +1,5 @@
+function home () {
+    return <h1>Obrigada Marco por ser meu caos criativo</h1>
+}
+
+export default home;
